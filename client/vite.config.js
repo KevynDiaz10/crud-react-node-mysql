6fc34ac8be4e061cff1efcdad6456e76a7213e77
kevyn <kevyndiaz10@gmail.com>
2024-09-06 13:10:49 -0400

    pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://KevynDiaz10.github.io/crud-front',
+  base: 'https://KevynDiaz10.github.io/crud-react-node-mysql',
 })
