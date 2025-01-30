7e8bedbda1c851e3771ccc1e6d828662dc039b19
KevynDiaz10 <111151756+KevynDiaz10@users.noreply.github.com>
2025-01-30 17:23:44 -0400

    Update BlogModel.js new coment from sequelize

--- node/models/BlogModel.js
+++ node/models/BlogModel.js
@@ -1,6 +1,6 @@
 // importacion de la conexion de la Base de Datos
 import db from "../database/db.js";
-// importacion de sequelize 
+// sequelize es un ORM para Node.js que facilita la interacción con bases de datos relacionales como MySQL, PostgreSQL, SQLite y SQL Server
 import { DataTypes } from "sequelize";
 //representacion de tabla de la base de datos
 
