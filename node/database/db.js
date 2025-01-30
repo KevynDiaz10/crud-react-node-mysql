e1bd73f01acc2a3ffe96ca60f313e3c745515abe
KevynDiaz10 <111151756+KevynDiaz10@users.noreply.github.com>
2025-01-30 17:24:30 -0400

    Update db.js comentario de sequelize

--- node/database/db.js
+++ node/database/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-// conexion a la base de batos 
+// conexion a la base de batos con sequelize que es un ORM
 const db = new Sequelize('registro_crud', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
